b336fc98af552d4f49a45c31ce808ebe3a7de690
KZ737 <59026913+KZ737@users.noreply.github.com>
2022-12-02 17:37:24 +0100

    day 2 explanations added

--- day_2/puzzle_1.py
+++ day_2/puzzle_1.py
@@ -1,5 +1,14 @@
 inputfile = open("./day_2/input.txt", "r")
 curSum = 0
 for line in inputfile:
+    '''
+    1, 2, or 3 points for the chosen shape
+    In the order of rock, paper, and scissors, each element is beaten by the next element, so if we subtract the value of our opponent's shape from ours, we get:
+        0 if we chose the same shape -> draw,
+        1 if I chose the one following theirs -> I win,
+        -1 if I chose the one which is followed by theirs -> they win.
+        
+    In languages where indexing with negative numbers is not possible, a mod 3 is needed for the indexing of the second array (turning -1 into 2)
+    '''
     curSum += [1, 2, 3][ord(line[2])-ord("X")] + [3, 6, 0][ord(line[2])-ord(line[0])-ord("X")+ord("A")]
 print(curSum)
--- day_2/puzzle_2.py
+++ day_2/puzzle_2.py
@@ -1,5 +1,10 @@
 inputfile = open("./day_2/input.txt", "r")
 curSum = 0
 for line in inputfile:
+    '''
+    1, 2, or 3 points to the winner, the principle is the same as in the first puzzle of today, just reversed:
+        we add the result (-1, 0, and 1 for lose, draw, and win, respectively) to the value of the shape chosen by our opponent to get the value of our shape (mod 3)
+    0, 3, or 6 points for the outcome, which is determined only by the second letter
+    '''
     curSum += [1, 2, 3][((ord(line[0])-ord("A"))+(ord(line[2])-ord("Y"))) % 3] + [0, 3, 6][ord(line[2])-ord("X")]
 print(curSum)
