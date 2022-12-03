33ae9f45196b241247837422ac4f9c6295f59012
KZ737 <59026913+KZ737@users.noreply.github.com>
2022-12-03 11:20:18 +0100

    day 3

---
+++ day_3/puzzle_1.py
@@ -0,0 +1,11 @@
+inputfile = open("./day_3/input.txt", "r")
+curSum = 0
+for line in inputfile:
+    '''
+    Get both halves of the line into sets and get their intersection to get the common element.
+    After which we simply calculate the priority of the common element.
+    '''
+    sacks = [set(list(line.strip()[:len(line)//2])), set(list(line.strip()[len(line)//2:]))]
+    commonElement = sacks[0].intersection(sacks[1]).pop()
+    curSum += (ord(commonElement) - ord("a") + 1) if commonElement.islower() else (ord(commonElement) - ord("A") + 27)
+print(curSum)
---
+++ day_3/puzzle_2.py
@@ -0,0 +1,14 @@
+inputfile = open("./day_3/input.txt", "r")
+curSum = 0
+sacks = []
+for line in inputfile:
+    '''
+    Get every triplet of lines into sets and get their intersection to get the common element. 
+    After we have 3 sacks, which we simply calculate the priority of the common element, then reset the sacks.
+    '''
+    sacks.append(set(list(line.strip())))
+    if len(sacks) == 3:
+        commonElement = sacks[0].intersection(sacks[1]).intersection(sacks[2]).pop()
+        curSum += (ord(commonElement) - ord("a") + 1) if commonElement.islower() else (ord(commonElement) - ord("A") + 27)
+        sacks = []
+print(curSum)
